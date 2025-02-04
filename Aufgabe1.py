4d1667447e0cc82ab324a46d2a7b02829ed0a8e0
shawtylissy <116068669+shawtylissy@users.noreply.github.com>
2025-02-04 14:28:49 +0100

    Update Aufgabe1.py

--- Aufgabe1.py
+++ Aufgabe1.py
@@ -1,8 +1,1 @@
-#Testaufgabe
-# Eingabe einer Zahl und Konvertierung in einen Integer
-zahl = int(input("Gebe eine Zahl an: "))
-
-# Ausgabe der gespeicherten Zahl
-print(f"Du hast die Zahl {zahl} angegeben :3")
-
-#Test
+#Testen des Workflows
