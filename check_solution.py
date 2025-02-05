5b7439815d87d1331c590544ffc9b67f58de8f93
shawtylissy <116068669+shawtylissy@users.noreply.github.com>
2025-02-05 16:36:23 +0100

    Update check_solution.py

--- check_solution.py
+++ check_solution.py
@@ -1,1 +1,43 @@
-print(1)
+import nbformat
+import subprocess
+
+notebookDatei = "Testbook.ipynb"  
+cellMarker = "###Aufgabe 1" 
+solution = "2" 
+
+def finde_loesung_zelle(notebookDatei, cellMarker):
+    with open(notebookDatei, 'r', encoding='utf-8') as f:
+        nb = nbformat.read(f, as_version=4)
+    
+    for cell in nb.cells:
+        if cell.cell_type == "code" and marker in cell.source:
+            return cell.source  
+    return None
+
+def fuehre_code_aus(code):
+    try:
+        result = subprocess.run(
+            ["python", "-c", code],  
+            capture_output=True, text=True, timeout=5
+        )
+        return result.stdout.strip()
+    except Exception as e:
+        return f"Fehler: {str(e)}"
+
+def schreibe_bewertung(output, erwartet):
+    with open("Bewertung.txt", "w", encoding="utf-8") as f:
+        if output == erwartet:
+            f.write("Das Ergebnis ist richtig.\n")
+        else:
+            f.write(f"Die Lösung ist falsch. Die richtige Lösung lautet: '{erwartet}' \n")
+
+if __name__ == "__main__":
+    code = finde_aufgaben_zelle(notebookDatei, cellMarker)
+    
+    if code:
+        output = fuehre_code_aus(code)
+        schreibe_bewertung(output, solution)
+    else:
+        with open("Bewertung.txt", "w", encoding="utf-8") as f:
+            f.write("Keine passende Zelle gefunden.\n")
+
