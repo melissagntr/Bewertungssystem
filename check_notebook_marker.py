a0d913ccb88192ff3d8dbebb2a2789240be77ee9
shawtylissy <116068669+shawtylissy@users.noreply.github.com>
2025-02-05 20:19:11 +0100

    Update check_notebook_marker.py

--- check_notebook_marker.py
+++ check_notebook_marker.py
@@ -1,9 +1,9 @@
 import nbformat
 
-notebook_file = "Testbook.ipynb" #Dateiname des Jupyter Notebooks mit studentischer Lösung
-search_marker = "###Aufgabe 1"
+notebook_file = "Testbook.ipynb"
+cell_marker = "###Aufgabe 1"
 
-def check_notebook_for_task(notebook_path, marker):
+def check_notebook_for_marker(notebook_path, marker):
     try:
         with open(notebook_path, "r", encoding="utf-8") as f:
             nb = nbformat.read(f, as_version=4)
@@ -16,12 +16,12 @@ def check_notebook_for_task(notebook_path, marker):
     except Exception as e:
         return f"Fehler beim Lesen des Notebooks: {e}"
 
-result = check_notebook_for_task(notebook_file, search_marker)
+result = check_notebook_for_marker(notebook_file, cell_marker)
 
 with open("result.txt", "w", encoding="utf-8") as f:
     if result is True:
-        f.write(f" Die Zelle mit '{search_marker}' wurde gefunden.\n")
+        f.write(f" Die Zelle mit '{cell_marker}' wurde gefunden.\n")
     elif result is False:
-        f.write(f" Die Zelle mit '{search_marker}' wurde NICHT gefunden.\n")
+        f.write(f" Die Zelle mit '{cell_marker}' wurde NICHT gefunden.\n")
     else:
         f.write(f" {result}\n")
