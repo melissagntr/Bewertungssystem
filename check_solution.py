0ebeb2eeff208895d1396bbfbb6daca3fca653b6
Melissa <melissa.guenther@stud.htwk-leipzig.de>
2025-02-07 12:54:35 +0100

    Anpassung beider Python-Dateien

--- check_solution.py
+++ check_solution.py
@@ -1,9 +1,16 @@
 import nbformat
 import subprocess
 
-notebook_file = "Testbook.ipynb"  
-cell_marker = "###Aufgabe 1" 
-solution = "2" 
+notebook_file = "Testbook.ipynb" #evtl. dynamisch gestalten: der Student muss den Dateinamen angeben
+# cell_marker = ["###Aufgabe 1", "###Aufgabe 2"]
+# solution = [2, "Hello World"]
+solution_per_task = {
+    "###Aufgabe 1": 2,
+    "###Aufgabe 2": "Hello World"
+}
+
+with open("Bewertung.txt", "w", encoding="utf-8") as f:
+    pass
 
 def finde_loesung_zelle(notebook, marker):
     with open(notebook, 'r', encoding='utf-8') as f:
@@ -25,19 +32,20 @@ def fuehre_code_aus(code):
         return f"Fehler: {str(e)}"
 
 def schreibe_bewertung(output, erwartet):
-    with open("Bewertung.txt", "w", encoding="utf-8") as f:
+    with open("Bewertung.txt", "a", encoding="utf-8") as f:
         if output == erwartet:
             f.write("Das Ergebnis ist richtig.\n")
         else:
             f.write(f"Die Lösung ist falsch. Die richtige Lösung lautet: '{erwartet}' \n")
 
-if __name__ == "__main__":
-    code = finde_loesung_zelle(notebook_file, cell_marker)
-    
-    if code:
-        output = fuehre_code_aus(code)
-        schreibe_bewertung(output, solution)
-    else:
-        with open("Bewertung.txt", "w", encoding="utf-8") as f:
-            f.write("Keine passende Zelle gefunden.\n")
+for key in solution_per_task:
+    if __name__ == "__main__":
+        code = finde_loesung_zelle(notebook_file, key)
+        
+        if code:
+            output = fuehre_code_aus(code)
+            schreibe_bewertung(output, solution_per_task[key])
+        else:
+            with open("Bewertung.txt", "a", encoding="utf-8") as f:
+                f.write("Keine passende Zelle gefunden.\n")
 
