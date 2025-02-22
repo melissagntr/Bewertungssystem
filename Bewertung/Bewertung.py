992292d3cb8bf3d82666c505fc004c578b4c54c5
Melissa <melissa.guenther@stud.htwk-leipzig.de>
2025-02-22 22:03:59 +0100

    Added counter for solution printing after 3 trials

--- Bewertung/Bewertung.py
+++ Bewertung/Bewertung.py
@@ -1,6 +1,7 @@
 import nbformat #Modul, das Jupyter-Notebooks als strukturiertes JSON-Format (Dictionary) speichert
 import subprocess
 import sys
+import json
 #import requests
 #URL = "http://localhost:8888"
 
@@ -31,15 +32,15 @@ def fuehre_code_aus(code):
         return result.stdout.strip()
     except Exception as e:
         return f"Fehler: {str(e)}"
-
+    
 #def erhalte_bewertung(code):
-    # headers = {"code": code}
+    # headers = {"code": code, "count": try_counter}
     # response = requests.post(URL, headers=headers)
 
     # result = response.text
     # return result
 
-def schreibe_bewertung(nb, index, output, erwartet):
+def schreibe_bewertung(nb, index, output, erwartet, counter):
     # Da der Output als String herauskommt
     try:
         output = eval(output)
@@ -48,8 +49,14 @@ def schreibe_bewertung(nb, index, output, erwartet):
     
     if output == erwartet:
         text = "Das Ergebnis ist korrekt."
+        counter += 1
     else:
-        text = "Das Ergebnis ist falsch."
+        if counter < 3:
+            text = "Das Ergebnis ist falsch."
+            counter += 1
+        else:
+            text = "Das Ergebnis ist falsch, das richige Ergebnis lautet: ."
+            counter += 1
     
     if index + 1 < len(nb.cells) and nb.cells[index + 1].cell_type == "markdown":
         if nb.cells[index + 1].source.startswith("Das Ergebnis ist"):  
@@ -61,24 +68,38 @@ def schreibe_bewertung(nb, index, output, erwartet):
         markdown_zelle = nbformat.v4.new_markdown_cell(text)
         nb.cells.insert(index + 1, markdown_zelle)
 
+    return counter
+
 
 def speichere_notebook(notebook_path, nb):
     with open(notebook_path, 'w', encoding='utf-8') as f:
         nbformat.write(nb, f)
 
-for key in solution_per_task:
-    if __name__ == "__main__":
+
+if __name__ == "__main__":
+
+    for key in solution_per_task:
+
+        with open("userconfig.json", "r") as file:
+            userconfig = json.load(file)
+
+        try_counter = userconfig[key]
+
         nb, index, code = suche_loesungs_zelle(notebook_datei, key)
             
         if code:
             output = fuehre_code_aus(code)
-            schreibe_bewertung(nb, index, output, solution_per_task[key])
+            new_count = schreibe_bewertung(nb, index, output, solution_per_task[key], try_counter)
             speichere_notebook(notebook_datei, nb)
             print("Bewertung wurde in das Notebook eingefügt.")
+
+            userconfig[key] = new_count
+            with open("userconfig.json", "w") as file:
+                json.dump(userconfig, file, indent=4)
+                
         else:
             print("Keine passende Zelle gefunden.")   
 
-#Testen der Anpassung des Workflows 
 
 
 
